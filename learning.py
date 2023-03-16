98bda7d814b98013d1c1702272dcc7f788f6358b
amxnese <aminesdrt42@gmail.com>
2023-03-16 17:39:40 +0000

    modified learning.py

--- learning.py
+++ learning.py
@@ -3015,3 +3015,11 @@
 # t = [x,f]
 # ziped = [*(itertools.zip_longest(*t))]
 # print(ziped)
+
+# 'sorting a dictionary'
+# dic = {}
+# for i in range(10,0,-1):
+#     dic[f"number{11-i}"] = i
+# sorted_dic = sorted(dic.items(),key=lambda x:x[1])
+# print(sorted_dic)
+
