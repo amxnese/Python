59d52db9ff703375cfd6664445d6ec887bda96fd
amine <108814041+amxnese@users.noreply.github.com>
2024-07-12 19:45:17 +0100

    created tricks folder with dics.py and vars.py files

---
+++ tricks/dics.py
@@ -0,0 +1,7 @@
+d1 = {'name':'amine','age':20}
+d2 = {'name':'alex','city':'Biskra'}
+merged_dic = {**d1,**d2}
+merged_dic = d1 | d2
+print(d1)
+print(d2)
+print(merged_dic)
---
+++ tricks/vars.py
@@ -0,0 +1,8 @@
+name = 'amin'
+a,b,c,d, = name
+print(a,b,c,d)
+
+fname = 'sedrata'
+lname = 'amine'
+fname, lname = lname, fname
+print(fname,lname)
