a7bdb1e50180c60074d192b43bbe2aa39957a6b5
amine <108814041+amxnese@users.noreply.github.com>
2024-07-12 19:45:44 +0100

    created testing folder with assert.py file in it

---
+++ testing/assert.py
@@ -0,0 +1,5 @@
+test.assert_equals(make_readable(0), "00:00:00")
+test.assert_equals(make_readable(5), "00:00:05")
+test.assert_equals(make_readable(60), "00:01:00")
+test.assert_equals(make_readable(86399), "23:59:59")
+test.assert_equals(make_readable(359999), "99:59:59")
